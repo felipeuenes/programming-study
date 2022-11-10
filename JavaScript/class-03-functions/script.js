e68fc9ec4e337a4bfc7314a54d3be7169ce99fba
Felipe Uenes <uenesdesigner@gmail.com>
2022-11-10 15:14:08 -0300

    add function

--- JavaScript/class-03-functions/script.js
+++ JavaScript/class-03-functions/script.js
@@ -6,7 +6,22 @@ myTest()
 myTest()
 
 function somar(num1, num2){
-    return num1 + num2;
+    
+    let soma = num1 + num2;
+    
+    return soma;
 }
 
 console.log(somar(20,30));
+
+
+// >>> Anonnymous <<<
+
+
+let sum = function (num1, num2){
+    
+    let soma = num1 + num2;   
+    return soma;
+}
+
+console.log(sum(2, 3));
