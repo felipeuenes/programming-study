d917994ade138309ee84640ce617cc03f5e16f17
Felipe Uenes <uenesdesigner@gmail.com>
2022-11-11 15:35:12 -0300

    add class 03

---
+++ JavaScript/class-03-functions/challenge/script.js
@@ -0,0 +1,22 @@
+function calcIMC(pacient){
+    return imc;
+}
+
+const listPacient = [
+    {
+        name: "Maria",
+        heigth: 169,
+        weigth: 60
+
+    },
+    {
+        name: "João",
+        heigth: 179,
+        weigth: 80
+
+    }
+]
+
+document.write(`O IMC de ${listPacient[0].name} é ${imc}`)
+
+let num = [1, "e", 3 ]
