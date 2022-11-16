0fde1f43ae64839f35cf94c9fed2b3bfd9ff8958
Felipe Uenes <uenesdesigner@gmail.com>
2022-11-16 15:02:50 -0300

    add class 04

---
+++ JavaScript/class-04-dataManipulation/script.js
@@ -0,0 +1,18 @@
+// Prototype
+
+function Car(motorPower){
+    this.motor = motorPower;
+
+}
+
+let myName = "Felipe";
+let age = 19;
+
+console.log(myName.__proto__);
+
+// diz quantos caracteres tem:
+console.log(myName.length);
+
+// pega a primeira letra:
+console.log(myName[0]);
+
