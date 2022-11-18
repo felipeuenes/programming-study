35a0a442c36df473854061b93787d97c523ba4f0
Felipe Uenes <uenesdesigner@gmail.com>
2022-11-18 16:22:24 -0300

    add challenge

--- JavaScript/class-04-dataManipulation/challenge/delete-array-element/script.js
+++ JavaScript/class-04-dataManipulation/challenge/delete-array-element/script.js
@@ -1,2 +1,18 @@
 let techs = ['HTML', 15, 'CSS', 15, 'JavaScript', 30, 'SQL', 10, 'Git',8]
 
+function deleteArrayElement (techName){
+    let auxArray = techs;
+
+    auxArray.map(element =>{
+        
+        document.write(`${element} <br>`)
+        
+        if(String(techName).toLocaleLowerCase()) == String(element).toLocaleLowerCase()){
+            window.alert(element)
+        }
+    })
+}
+
+deleteArrayElement("CSS")
+document.write(techs);
+
