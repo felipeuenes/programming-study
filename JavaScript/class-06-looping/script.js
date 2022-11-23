522ae7705577a0f7f454bb0caf351c253aa8f1b6
Felipe Uenes <uenesdesigner@gmail.com>
2022-11-23 15:14:25 -0300

    add bug

--- JavaScript/class-06-looping/script.js
+++ JavaScript/class-06-looping/script.js
@@ -5,11 +5,16 @@
 
 function calcular(){
     
-    let nota1 = document.getElementById("n1").value;
-    let nota2 = document.getElementById("n2").value;
+    let nota1 = Number(document.getElementById("n1").value);
+    let nota2 = Number(document.getElementById("n2").value);
 
     let resultado = (nota1 + nota2) / 2;
 
     document.write(`Sua média é ${resultado}`)
+
+    while (resultado < 6){
+        document.write("Reprovado")
+        
+    }
 
 }
