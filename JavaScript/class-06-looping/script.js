e91f4224427cc02610aa0485acc6bb5a19979001
Felipe Uenes <uenesdesigner@gmail.com>
2022-11-23 15:50:14 -0300

    add

--- JavaScript/class-06-looping/script.js
+++ JavaScript/class-06-looping/script.js
@@ -8,13 +8,22 @@ function calcular(){
     let nota1 = Number(document.getElementById("n1").value);
     let nota2 = Number(document.getElementById("n2").value);
 
+    let nome = getElementById("name").value;
+
     let resultado = (nota1 + nota2) / 2;
 
-    document.write(`Sua média é ${resultado}`)
+    document.write(`${nome}Sua média é ${resultado}`)
 
-    while (resultado < 6){
-        document.write("Reprovado");
+    // while (resultado < 6){
+    //     document.write("Reprovado");
 
-    }
+    // }
 
+}
+
+
+do {
+    document.write("Quer fazer uma avaliação do sistema?")
+
+    
 }
