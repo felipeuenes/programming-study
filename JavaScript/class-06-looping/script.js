1de69e7e951f801851539aa52e8c2e554017dd61
Felipe Uenes <uenesdesigner@gmail.com>
2022-11-25 15:32:02 -0300

    add

--- JavaScript/class-06-looping/script.js
+++ JavaScript/class-06-looping/script.js
@@ -1,35 +1,41 @@
-Loopings
+// Loopings
 
 
-while
+// while
 
-function calcular(){
+// function calcular(){
     
-    let nota1 = Number(document.getElementById("n1").value);
-    let nota2 = Number(document.getElementById("n2").value);
+//     let nota1 = Number(document.getElementById("n1").value);
+//     let nota2 = Number(document.getElementById("n2").value);
 
-    let nome = getElementById("name").value;
+//     let nome = getElementById("name").value;
 
-    let resultado = (nota1 + nota2) / 2;
+//     let resultado = (nota1 + nota2) / 2;
 
-    document.write(`${nome}Sua média é ${resultado}`)
+//     document.write(`${nome}Sua média é ${resultado}`)
 
-    while (resultado < 6){
-        document.write("Reprovado");
+//     while (resultado < 6){
+//         document.write("Reprovado");
 
-    }
+//     }
 
-}
-
-
+// }
 
 
 
-for (let count = 0; count <= 10; count++) {
-    if (count % 2 == 1){
-        console.log(count);
-    }
+
+
+// for (let count = 0; count <= 10; count++) {
+//     if (count % 2 == 1){
+//         console.log(count);
+//     }
 
     
     
+// }
+
+let resp = true;
+
+while (resp){
+    resp = prompt("Digite seu nome");
 }
