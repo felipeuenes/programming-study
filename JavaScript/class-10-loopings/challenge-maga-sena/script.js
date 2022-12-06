8420a166d81293d39d34e06cc499bebc4c05375e
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-06 15:26:19 -0300

    mega sena

--- JavaScript/class-10-loopings/challenge-maga-sena/script.js
+++ JavaScript/class-10-loopings/challenge-maga-sena/script.js
@@ -1,6 +1,19 @@
-const numberMega = []
+const numberMega = [];
 
 
-let numberDrawn = Math.ceil(Math.random() * 100);
 
-document.write(numberDrawn);
+while (numberMega.length < 6) {
+    let numberDrawn = Math.ceil(Math.random() * 100);
+
+    if (!numberMega.includes(numberDrawn)) {
+        numberMega.push(numberDrawn);
+        
+    }
+
+    
+} 
+
+document.write(numberMega);
+
+
+
