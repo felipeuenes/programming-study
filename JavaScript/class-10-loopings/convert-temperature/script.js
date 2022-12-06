ac9ec0f338ea47c34fe0755736c7e3f21cbdc65c
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-06 16:08:36 -0300

    add aula

---
+++ JavaScript/class-10-loopings/convert-temperature/script.js
@@ -0,0 +1,13 @@
+
+function degreeConvert(degree) {
+    return degree
+    
+}
+
+
+try {
+    let temperature = "10C"
+console.log(`${temperature.toUpperCase()} = ${degreeConvert(temperature)}`);
+} catch (error){
+    console.log(error);
+}
