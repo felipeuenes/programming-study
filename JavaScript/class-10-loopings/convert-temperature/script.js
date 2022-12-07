d143fd72f017c8ee09fde3cbb146113012d7eed2
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-07 15:01:01 -0300

    add aula

--- JavaScript/class-10-loopings/convert-temperature/script.js
+++ JavaScript/class-10-loopings/convert-temperature/script.js
@@ -1,5 +1,29 @@
 
 function degreeConvert(degree) {
+   
+    let arrayDegree = Array.from(degree)
+    let scaleDegree = arrayDegree.pop()
+    let newDegree = Number(arrayDegree.join(""));
+
+
+
+
+    console.log(arrayDegree);
+    console.log(scaleDegree);
+    console.log(newDegree);
+
+
+
+    
+
+    if (scaleDegree == "C") {
+        let result = newDegree * 9/5 + 32;
+        return result
+   }
+   if (scaleDegree == "F") {
+    
+   }
+   
     return degree
     
 }
