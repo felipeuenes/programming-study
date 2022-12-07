f49e30c2f266a4916a6faa83b3ff34cba1b8dd60
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-07 15:40:03 -0300

    add converter temperature

--- JavaScript/class-10-loopings/convert-temperature/script.js
+++ JavaScript/class-10-loopings/convert-temperature/script.js
@@ -1,30 +1,34 @@
 
 function degreeConvert(degree) {
-   
-    let arrayDegree = Array.from(degree)
+
+
+    let auxDegree = String(degree.split(" ").join(""))
+    let arrayDegree = Array.from(auxDegree)
     let scaleDegree = arrayDegree.pop()
     let newDegree = Number(arrayDegree.join(""));
 
 
 
-
+    console.log(auxDegree);
     console.log(arrayDegree);
     console.log(scaleDegree);
     console.log(newDegree);
 
+    if (!newDegree) {
+        throw new Error("Insira uma temperatura válida")
+    }
 
 
 
-
-    if (scaleDegree == "C") {
-        return (newDegree * 9/5 + 32) + "F";
+    if (scaleDegree.toUpperCase() == "C") {
+        return (newDegree * 9/5 + 32).toLocaleString("pt-br") + "F";
         // console.log(result);
         // document.write(`A conversão de ${newDegree}C para F é ${result}`)
         
         
    }
-   if (scaleDegree == "F") {
-        return ((newDegree - 32) * 5/9) + "C";
+   if (scaleDegree.toUpperCase() == "F") {
+        return ((newDegree - 32) * 5/9).toLocaleString("pt-br") + "C";
         // console.log(result2);
         // document.write(`A conversão de ${newDegree}F para C é ${result2}`)
    }
@@ -35,11 +39,8 @@ function degreeConvert(degree) {
 
 
 try {
-    let temperature = "50F"
-console.log(`${temperature.toUpperCase()} = ${degreeConvert(temperature)}`);
-
-// degreeConvert(temperature)
-// console.log(degreeConvert(temperature));
+    let temperature = "10f"
+console.log(`${temperature.toLowerCase()} = ${degreeConvert(temperature)}`);
 
 } catch (error){
     console.log(error);
