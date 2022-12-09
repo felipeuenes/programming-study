82471cf6f32db98d862dc9398e9d0c5141cbafa0
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-08 21:20:25 -0300

    add challenge

---
+++ JavaScript/challenges/script.js
@@ -0,0 +1,26 @@
+// Faça um Menu com as seguintes opções:
+/*
+    Digite a opção desejada:
+
+    1 - Cadastrar item
+    2 - Remover item cadastrado
+    3 - Ver itens cadastrados
+    4 - Sair do programa
+*/
+
+// Fazer alerta de item cadastrado caso o mesmo já esteja cadastrado
+// Se não houver itens cadastrados mostrar mensagem de alerta nas opções
+// Mostrar mensagem de sucesso junto do nome ao cadastrar ou remover ítem
+// O programa também deve ser encerrado em caso do usuário não digitar valor.
+
+
+
+
+
+function cadas() {
+    let input = document.querySelector("#item");
+    let items = input.value;
+    console.log(items);
+    
+    document.write(items);
+}
