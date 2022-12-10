aa6d675c5afbd2e5ea50f0fbd1e6194cf010944a
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-09 23:41:57 -0300

    add

--- JavaScript/challenges/script.js
+++ JavaScript/challenges/script.js
@@ -14,13 +14,23 @@
 // O programa também deve ser encerrado em caso do usuário não digitar valor.
 
 
+let item = window.prompt("Digite a opção desejada: 1 - Cdastrar item 2 - Remover item cadastrado 3 - Ver itens cadastrados 4 - Sair do programa")
 
 
+console.log(item);
 
-function cadas() {
-    let input = document.querySelector("#item");
-    let items = input.value;
-    console.log(items);
-    
-    document.write(items);
+if (item == 1) {
+   
 }
+
+
+// function cadas() {
+//     let input = document.querySelector("#item");
+//     let items = input.value;
+//     console.log(items);
+    
+//     document.write(items);
+// }
+
+
+
