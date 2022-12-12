19cfbaedfd0902a6e6fee2b8de07c467f8523f77
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-12 15:06:47 -0300

    aula

--- JavaScript/challenges/script.js
+++ JavaScript/challenges/script.js
@@ -14,51 +14,67 @@
 // O programa também deve ser encerrado em caso do usuário não digitar valor.
 
 
-let item = window.prompt(`Digite a opção desejada:
-================
-1 - Cadastrar item
-2 - Remover item cadastrado
-3 - Ver itens cadastrados
-4 - Sair do programa`)
+// let item = window.prompt(`Digite a opção desejada:
+// ================
+// 1 - Cadastrar item
+// 2 - Remover item cadastrado
+// 3 - Ver itens cadastrados
+// 4 - Sair do programa`)
 
-console.log(item);
+// console.log(item);
 
-let banco = [];
+// let banco = [];
 
-switch (item) {
-    case "1":
-        let itemCadas = prompt("Digite o item para cadastrar")
+// switch (item) {
+//     case "1":
+//         let itemCadas = prompt("Digite o item para cadastrar")
         
-        banco.push(itemCadas);
+//         banco.push(itemCadas);
 
-        alert(`Item ${itemCadas} cadastrado!`)
-        let cadastroOutro = prompt("Deseja cadastrar outro? (responda sim ou não)")
+//         alert(`Item ${itemCadas} cadastrado!`)
+        
+//         let cadastroOutro = prompt("Deseja cadastrar outro? (responda sim ou não)")
+        
+// while (cadastroOutro == "sim") {
+    
+// }
+//     break;
+//     case "1":
+        
+//         break;
+//     case "2":
+        
+//         break;
+//     case "3":
+        
+//         break;
+//     case "4":
+        
+//         break;
 
-        if (cadastroOutro == "sim") {
-            
-        } else {
-            
-        }
-        break;
-    case "1":
-        
-        break;
-    case "2":
-        
-        break;
-    case "3":
-        
-        break;
-    case "4":
-        
-        break;
+//     default:
+//         break;
+// }
 
-    default:
-        break;
+// console.log(banco);
+
+const listItens = []
+let menuOption
+while (true) {
+    
+    
+    let menuOption = prompt(`Digite a opção desejada:
+    ================
+    1 - Cadastrar item
+    2 - Remover item cadastrado
+    3 - Ver itens cadastrados
+    4 - Sair do programa`)
+    
+    if (menuOption == '4') {
+        break
+    }
 }
-
-console.log(banco);
-
+alert("Programa encerrado")
 
 
 
