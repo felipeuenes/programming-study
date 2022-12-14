d80234ae523c64ece15f9b7fc52bdb2167ecba3d
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-14 13:00:22 -0300

    challenge

--- JavaScript/challenges/calcular-media/script.js
+++ JavaScript/challenges/calcular-media/script.js
@@ -1,8 +1,41 @@
 
 
-function calc() {
-    let nome = document.querySelector("#name");
+// function calc() {
+//     let nome = document.querySelector("#name");
+    
+//     console.log(nome);
+    
+// }
+while (true) {
+    
+    alert("Vamos calcular a média anual do aluno")
+    
+    let nome = prompt("Digite seu nome")
+    
+    let n1 = Number(prompt("Digite a primeira nota:"))
+    if (n1 > 10) {
+        alert("Nota máxima é 10")
+    }
+    let n2 = Number(prompt("Digite a segunda nota:"))
+    if (n2 > 10) {
+        alert("Nota máxima é 10")
+    }
+    let n3 = Number(prompt("Digite a terceira nota:"))
+    if (n3 > 10) {
+        alert("Nota máxima é 10")
+    }
+
+    let n4 = Number(prompt("Digite a quarta nota:"))
+    if (n4 > 10) {
+        alert("Nota máxima é 10")
+    }
+    
+    let media = (n1 + n2 + n3 + n4) / 4;
+    
+    console.log(media);
+    alert(`A média de ${nome} é ${media}`)
     
     console.log(nome);
-    
 }
+    alert("Programa encerrado")
+    
