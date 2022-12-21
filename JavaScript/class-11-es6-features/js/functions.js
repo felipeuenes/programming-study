172a3b88d38e9e8fa4ec2afdabff0c9e1a16f157
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-21 15:45:50 -0300

    add object

--- JavaScript/class-11-es6-features/js/functions.js
+++ JavaScript/class-11-es6-features/js/functions.js
@@ -1,6 +1,23 @@
-export function sum(num1, num2) {
+function sum(num1, num2) {
     console.log(num1 + num2);
+}
+function sub(num1, num2) {
+    console.log(num1 - num2);
+}
+function mult(num1, num2) {
+    console.log(num1 * num2);
+}
+function div(num1, num2) {
+    console.log(num1 / num2);
 }
 
 
-sum(1, 2)
+
+// export {sum, sub, mult, div};
+
+export const operations = ({
+    sum,
+    sub,
+    mult,
+    div,
+})
--- JavaScript/class-11-es6-features/js/script.js
+++ JavaScript/class-11-es6-features/js/script.js
@@ -1,3 +1,10 @@
-import { sum } from "./functions.js";
+// import { sum, mult, sub, div } from "./functions.js";
+import { operations } from "./functions.js";
 
-sum(2, 6);
+
+operations.mult(2, 2)
+
+// sum(2, 6);
+// sub(5, 5);
+// div(8, 5);
+// mult(2, 2);
