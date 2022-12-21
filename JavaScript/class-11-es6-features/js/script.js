7b5c2a294feb41fe4cca11e9508dfa37f8d9e752
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-21 16:00:58 -0300

    person1

--- JavaScript/class-11-es6-features/js/script.js
+++ JavaScript/class-11-es6-features/js/script.js
@@ -2,9 +2,25 @@
 import { operations } from "./functions.js";
 
 
-operations.mult(2, 2)
+// operations.mult(2, 2)
+// operations.sub(2, 2)
+// operations.sum(2, 2)
+// operations.div(2, 2)
 
 // sum(2, 6);
 // sub(5, 5);
 // div(8, 5);
 // mult(2, 2);
+
+class Person {
+    constructor(name, age){
+        this.name = name,
+        this.age = age
+    }
+}
+
+
+const person1 = new Person('Felipe', 30);
+
+console.log(person1.name);
+console.log(person1.age);
