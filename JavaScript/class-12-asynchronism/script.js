0066aff0cb763260d209cb4d7b2c5339cf6b272e
Felipe Uenes <uenesdesigner@gmail.com>
2022-12-23 15:27:07 -0300

    add class

--- JavaScript/class-12-asynchronism/script.js
+++ JavaScript/class-12-asynchronism/script.js
@@ -1,32 +1,77 @@
-console.log('Início');
-setTimeout(() => {
-  console.log('Lógica')},  1000)
-console.log('Fim');
+// console.log('Início');
+// setTimeout(() => {
+//   console.log('Lógica')},  1000)
+// console.log('Fim');
 
-setInterval(() => {
-    console.log('SetInterval');
-}, 6500)
+// setInterval(() => {
+//     console.log('SetInterval');
+// }, 6500)
 
 
 
-const arrayNumbers = [12,20,30,45,66,70];
-const arrayAux = [];
-let count = 0;
-let int = setInterval(() => {
-    arrayAux.push(arrayNumbers[count]);
-    const numbers = document.querySelector('.numbers');
-    numbers.innerHTML = arrayAux;
+// const arrayNumbers = [12,20,30,45,66,70];
+// const arrayAux = [];
+// let count = 0;
+// let int = setInterval(() => {
+//     arrayAux.push(arrayNumbers[count]);
+//     const numbers = document.querySelector('.numbers');
+//     numbers.innerHTML = arrayAux;
 
-    if (arrayAux.length == arrayNumbers.length) {
-        clearTimeout(out);
-        clearInterval(int);
+//     if (arrayAux.length == arrayNumbers.length) {
+//         clearTimeout(out);
+//         clearInterval(int);
+//     }
+//     count++
+// }, 1500);
+
+
+// let out = setTimeout(() => {
+//     console.log('Fim');
+// })
+
+
+// >>>>>> Promises <<<<<<<<
+
+console.log('Fazer requisição');
+
+
+
+const promise = new Promise((resolve, reject) => {
+
+let request = true
+
+    if (request) {
+        return resolve('Requisição aceita')
+    } else {
+        return reject('requisição não aceita')
     }
-    count++
-}, 1500);
 
 
-let out = setTimeout(() => {
-    console.log('Fim');
 })
 
+console.log(promise);
 
+// promise
+// .then((answer) => console.log(answer))
+// .catch((error) => console.log(error))
+// .finally(() => console.log('Requisição encerrada!'));
+
+
+
+async function requestData() {
+    try {
+        const apromise = await promise; 
+        console.log(promise);  
+        
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log("Requisição encerrada");
+    }
+   
+}
+
+requestData()
+
+
+// console.log('Finalizar requisição');
