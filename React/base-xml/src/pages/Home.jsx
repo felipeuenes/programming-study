d5e3b91eae57b05a0f82b74bee32e61f97d78892
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-03 16:24:15 -0300

    add class react

--- React/base-xml/src/pages/Home.jsx
+++ React/base-xml/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 
 import './Home.css';
-import Icon from "../images/react.svg"
+import IconReact from "../images/react.svg"
 
 export function Home() {
   
   return (
     <div className='container'>
-      <img src={Icon} alt="" srcset="" />
       <h1> Class React Base JSX</h1>
+      <img src={IconReact} alt="" srcset="" />
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet est provident, blanditiis, maxime aperiam maiores nihil deserunt reiciendis, aspernatur pariatur laboriosam. Nobis, corporis. Esse, placeat. Consectetur libero illum itaque qui.</p>
 
      
