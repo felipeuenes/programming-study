52b494c897a44c90b566bc4c1083d25aa6bd2571
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-03 16:07:33 -0300

    react

---
+++ React/base-xml/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+
+import './Home.css';
+import Icon from "../images/react.svg"
+
+export function Home() {
+  
+  return (
+    <div className='container'>
+      <img src={Icon} alt="" srcset="" />
+      <h1> Class React Base JSX</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet est provident, blanditiis, maxime aperiam maiores nihil deserunt reiciendis, aspernatur pariatur laboriosam. Nobis, corporis. Esse, placeat. Consectetur libero illum itaque qui.</p>
+
+     
+    </div>
+  )
+}
+
+
