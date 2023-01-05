9ebb7f0b428e9e607085c4f8bc1637ca5188e462
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-05 15:36:27 -0300

    add button

--- React/class-03-hooks/src/pages/App.jsx
+++ React/class-03-hooks/src/pages/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 
 import './App.css'
 
 export function App() {
+  const [name, setName] = useState('Felipe');
+  const [count, setCount] = useState(0);
   
+  
+  console.log(name);
+
   return (
    <div className='container'>
-    <h1>Class Hooks</h1>
+     <h1>Class Hooks</h1>
+      <p>Nome: {name}</p>
+      <p>Número: {count}</p>
+      <button onClick={() => setCount(count + 2)}>Contar</button>
+
    </div>
   )
 }
