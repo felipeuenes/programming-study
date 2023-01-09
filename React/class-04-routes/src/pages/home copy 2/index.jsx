44c7d45d8c3c34133b660ac23253dd23c7a62b8a
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-09 15:36:32 -0300

    add animate react

---
+++ React/class-04-routes/src/pages/home copy 2/index.jsx
@@ -0,0 +1,21 @@
+
+import reactLogo from '../../assets/react.svg'
+import './style.css'
+
+export function Home() {
+ 
+
+  return (
+    <div className="container">
+      <h1>Home</h1>
+
+      <img src={reactLogo} alt="" srcset="" />
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam enim eius exercitationem, explicabo dolorum eligendi quia, asperiores laudantium voluptate harum nobis, nam dolores perspiciatis maxime. Veritatis soluta necessitatibus id voluptatibus!</p>
+
+    
+    </div>
+  )
+}
+
+
