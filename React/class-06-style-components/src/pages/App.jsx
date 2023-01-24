7c1635e9daeee12178309b715ef2855b64997d24
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-24 15:29:05 -0300

    styled component

---
+++ React/class-06-style-components/src/pages/App.jsx
@@ -0,0 +1,15 @@
+
+import { Container } from "./style"
+
+
+export function App() {
+  
+
+  return (
+    <Container>
+        <h1>Class Style Components</h1>
+    </Container>
+  )
+}
+
+
---
+++ React/class-06-style-components/src/pages/style.js
@@ -0,0 +1,12 @@
+// CSS-in-JS;
+import styled from 'styled-components'
+
+
+export const Container = styled.div `
+  background: #252525;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+
+  height: 100vh;
+  
+`
