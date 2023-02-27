3bddb93b07077d3bba18ca35dcac9197a6c6ed02
Felipe Uenes <uenesdesigner@gmail.com>
2023-02-27 16:15:05 -0300

    add

---
+++ NodeJS/class-06-api-rest/src/controllers/users.js
@@ -0,0 +1,30 @@
+module.exports = {
+    async read (req, res){
+        try {
+            
+        } catch (error) {
+            return res.send(error)
+        }
+    },
+    async create (req, res){
+        try {
+            
+        } catch (error) {
+            return res.send(error)
+        }
+    },
+    async update (req, res){
+        try {
+            
+        } catch (error) {
+            return res.send(error)
+        }
+    },
+    async delete (req, res){
+        try {
+            
+        } catch (error) {
+            return res.send(error)
+        }
+    }
+}
--- NodeJS/class-06-api-rest/src/routes/index.js
+++ NodeJS/class-06-api-rest/src/routes/index.js
@@ -1,7 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
+const userControllers = require('../controllers/users.js')
 
+router.get('/user', userControllers.read);
+router.post('/user', userControllers.create);
+router.put('/user/:id', userControllers.update);
+router.delete('/user/:id', userControllers.delete);
 
 
 
