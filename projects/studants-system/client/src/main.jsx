fdc5fe589b0293163d7ca240a038a9ba4ce93d04
Felipe Uenes <uenesdesigner@gmail.com>
2023-03-06 16:24:44 -0300

    add theme

--- projects/studants-system/client/src/main.jsx
+++ projects/studants-system/client/src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import ResetStyle from './styles/reset'
 import { ThemeProvider } from 'styled-components'
-
+import { Theme } from './styles/theme'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={}>
+    <ThemeProvider theme={Theme}>
     <App />
     <ResetStyle/>
 
---
+++ projects/studants-system/client/src/styles/theme.js
@@ -0,0 +1,24 @@
+export const Theme = {
+    colors: {
+        main: '#024040',
+        complementary: "#401800",
+        background: '#252525',
+        neutral0: '#000',
+        neutral1: '#111',
+        neutral2: '#222',
+        neutral3: '#333',
+        neutral4: '#444',
+        neutral5: '#555',
+        neutral6: '#666',
+        neutral7: '#777',
+        neutral8: '#888',
+        neutral9: '#999',
+        neutralA: '#aaa',
+        neutralB: '#bbb',
+        neutralC: '#ccc',
+        neutralD: '#ddd',
+        neutralE: '#eee',
+        neutralF: '#fff',
+
+    }
+}
