2f14cd230d129b2013aab4a9040eb913b23770c1
Felipe Uenes <uenesdesigner@gmail.com>
2023-03-06 16:15:42 -0300

    class projects

--- projects/studants-system/client/src/main.jsx
+++ projects/studants-system/client/src/main.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import ResetStyle from './styles/reset'
+import { ThemeProvider } from 'styled-components'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={}>
     <App />
     <ResetStyle/>
+
+    </ThemeProvider>
   </React.StrictMode>,
 )
